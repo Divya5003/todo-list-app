c5983b9d4e24a746761ba7061e4cf711700f2ed7
Divya5003 <divyamahajan5003@gmail.com>
2024-08-04 18:07:55 +0530

    created api's

---
+++ pages/api/add-data.js
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    const { title, description, timestamp } = req.body;
+
+    const filePath = path.join(process.cwd(), 'data.json');
+    const jsonData = fs.readFileSync(filePath);
+    const data = JSON.parse(jsonData);
+
+    const newEntry = { title, description, timestamp, isDone: false };
+    data.push(newEntry);
+
+    fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+
+    res.status(201).json(newEntry);
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+}
---
+++ pages/api/update-data.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import path from 'path';
+
+export default function handler(req, res) {
+    if (req.method === 'PUT') {
+        const { title, description, timestamp, isDone } = req.body;
+
+        const filePath = path.join(process.cwd(), 'data.json');
+        const jsonData = fs.readFileSync(filePath);
+        const data = JSON.parse(jsonData);
+
+        const entryIndex = data.findIndex((entry) => entry.title === title);
+
+        if (entryIndex !== -1) {
+            data[entryIndex] = { title, description, timestamp, isDone };
+            fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+            res.status(200).json(data[entryIndex]);
+        }
+        else {
+            res.status(404).json({ message: 'Not Found' });
+        }
+    } else {
+        res.status(405).json({ message: 'Method Not Allowed' });
+    }
+}
